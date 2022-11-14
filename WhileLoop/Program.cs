6c76973ce8e7212f729ece3abfcb0911e5f0ebe1
Alyar Mammadov <mammadovalyar@gmail.com>
2022-11-14 17:09:35 +0400

    loop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number less than 1000 and I will count for you");
+            number = Convert.ToInt16(Console.ReadLine());
+            while(number<=10000 )
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+        }
+    }
+}
