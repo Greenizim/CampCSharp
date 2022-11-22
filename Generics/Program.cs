cab7b20b7b840747f84c5fd0bb9e5e9d85261630
Alyar Mammadov <mammadovalyar@gmail.com>
2022-11-22 16:04:08 +0400

    Generic List

---
+++ Generics/Program.cs
@@ -0,0 +1,38 @@
+﻿List<string> cits = new List<string>();
+cits.Add("Baku");
+cits.Add("Baku");
+cits.Add("Baku");
+cits.Add("Baku");
+Console.WriteLine(cits.Count);
+MyList<string> citys2 = new MyList<string>();
+citys2.Add("Baku");
+citys2.Add("Baku");
+citys2.Add("Baku");
+Console.WriteLine(citys2.Count);
+#region Generic Class
+class MyList<T>//Generic class
+{
+    T[] _array;
+    T[] _tempArray;
+    public MyList()
+    {
+
+    }
+    public void Add(T item)
+    {
+        _tempArray = _array;
+        _array = new T[_array.Length + 1];
+        for (int i = 0; i < _tempArray.Length; i++)
+        {
+            _array[i] = _tempArray[i];
+        }
+        _array[_array.Length - 1]=item;
+    }
+    
+    //propfull
+    public int Count
+    {
+        get { return _array.Length; }
+    }
+}
+#endregion Generic Class
