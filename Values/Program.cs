7dc2a218f5c7d266d1d3c4633e444a991ad28685
Alyar Mammadov <mammadovalyar@gmail.com>
2022-11-22 11:48:41 +0400

    Length of Values

---
+++ Values/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+*    1.              byte : Uzunluğu 1 byte’tır, 0 ile 255 arasında değer alır.         
+*    2.              sbyte : Uzunluğu 1 byte’tır, -128 ile 127 arasında değer alır.         
+*    3.              short : Uzunluğu 2 byte’tır, -32768 ile 32767 arasında değer alır.         
+*    4.              ushort : Uzunlupu 2 byte’tır, 0 ile 65535 arasında değer alır.         
+*    5.              int : Uzunluğu 4 byte’tır, -2.147.483.648 ile 2.147.483.648 arasında değer alır.         
+*    6.              uint : Uzunluğu 4 byte’tır, 0 ile 4.294.967.295 arasında değer alır.         
+*    7.              long : Uzunluğu 8 byte’tır, -1020 ile 1020 arasında değer alır.         
+*    8.              ulong : Uzunluğu 8 byte’tır, 0 ile 2 x 1020 arasında değer alır.         
+*    9.              float : Uzunluğu 4 byte’tır, 1.5 x 10 - 45 ile 3.4 x 1038 arasında değer alır.         
+*    10.              double : Uzunluğu 8 byte’tır, 5.0 x 10 - 324 ile 1.7 x 10308 arasında değer alır.         
+*    11.              decimal : Uzunluğu 12 byte’tır, ±1.0 x 10 - 28 ile ±7.9 x 1028 arasında değer alır.         
+*    12.              char : Uzunluğu 2 byte’tır, Bütün ınicode karakterleri kapsar.         
+*    13.              string : Tek bir karakter, sözcük veya cümle gibi değerlerin saklanmasında kullanılır.         
+*    14.              boolean : True – false değer tutan tiptir.
+*/
