08860ded245c51d36605922a18100611f40af8a7
Alyar Mammadov <mammadovalyar@gmail.com>
2022-11-23 17:11:11 +0400

    Dictionary Of Generic Type

---
+++ DictionaryList/Program.cs
@@ -0,0 +1,7 @@
+﻿Dictionary<string,string> CapitalsOfGovernments = new Dictionary<string,string>();
+CapitalsOfGovernments.Add("Azerbaijan", "Baku");
+CapitalsOfGovernments.Add("Turkey", "Ankara");
+Console.WriteLine(CapitalsOfGovernments.Count);
+CapitalsOfGovernments.Add("Pakistan", "Islamabad");
+Console.WriteLine(CapitalsOfGovernments["Turkey"]);
+
