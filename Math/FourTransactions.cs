8650a665b1ca0eea5d0b2d74adb0e87b2f3d677b
Alyar Mammadov <mammadovalyar@gmail.com>
2022-11-23 17:09:32 +0400

    Homeworks

--- Math/FourTransactions.cs
+++ Math/FourTransactions.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Riyaziyyat
+namespace Math
 {
     internal class FourTransactions
     {
         public void Topla(int number1, int number2)
-        { 
+        {
             //Topla(2,3)
             int toplam = number1 + number2;
             Console.WriteLine("Netice : " + toplam);
--- Math/Program.cs
+++ Math/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Riyaziyyat
+namespace Math
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          FourTransactions dordIslem = new FourTransactions();
+            FourTransactions dordIslem = new FourTransactions();
             dordIslem.Topla(5, 6);
 
             dordIslem.Topla(8, 9);
