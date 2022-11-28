85a5c880e61a7ab56f217b4f0385b73752e3bdf7
Alyar Mammadov <mammadovalyar@gmail.com>
2022-11-28 09:23:29 +0400

    Entity, Inheritance, Solid

--- OOP2/CustomerManager.cs
+++ OOP2/CustomerManager.cs
@@ -8,13 +8,14 @@ namespace OOP2
     internal class CustomerManager
     {
         //parametr yazaq
-        public void Add(Customer customer)
+        public void Add(Customer customer)//customer uzerinden musteriyi gonderebilmek polymorphism dir base sinif uzerinden islediyimiz ucun hem ferdi hemde korperativ musteriyi tuta bilirik
         {
             //biz korperativ ve ferdi musteriyide buraya getirmeliyik bunun ucun ferdiye kecek
             //if (customer.)
             //{
 
             //}
+            //eger senin bir base sinifin varsa bu sinif referans tutucudur
         }
     }
 }
