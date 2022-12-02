e89777ac3675e4982f07e552ecd9541da0c55910
Alyar Mammadov <mammadovalyar@gmail.com>
2022-12-02 16:33:09 +0400

    Class,Property

--- PropertyInClass/Program.cs
+++ PropertyInClass/Program.cs
@@ -7,7 +7,18 @@ customerManager.Update();
 ProductManager productManager = new ProductManager();
 productManager.Add();
 productManager.Update();
+#endregion Classes
 
+#region Class Availability
+Customer customer2 = new Customer{Id = 1, FirstName = "George", LastName = "Reynold", City = "Tbilisi"};
+
+Customer customer1 = new Customer();
+customer1.Id = 1;
+customer1.FirstName = "Mike";
+customer1.LastName = "Tyson";
+customer1.City = "Ottowa";
+Console.WriteLine(customer2.LastName);
+#endregion Class Availability
 #region Property In Customer
-int a;
+
 #endregion Property In Customer
