b4dce95c1f3969c93b9f5457e2f96d762b9152be
Alyar Mammadov <mammadovalyar@gmail.com>
2022-12-06 10:44:18 +0400

    Interface

---
+++ Interfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿interface IPerson // konkret obyekt
+{
+    int Id { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }   
+}
+
+class Customer : IPerson//mucerred obyekt
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
+
+class Student : IPerson//mucerred obyekt
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Adress { get; set; }
+}
