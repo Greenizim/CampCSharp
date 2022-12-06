972b540ee2239fa29aa6625ba2a1a12a663ae237
Alyar Mammadov <mammadovalyar@gmail.com>
2022-12-06 12:01:06 +0400

    Interface, Interface Usage

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,4 +1,16 @@
-﻿interface IPerson // konkret obyekt
+﻿
+PersonManager manager = new PersonManager();
+Customer customer = new Customer();
+manager.Add(new Customer { Id = 1, FirstName = "Mike", LastName = "Tyson", City = "Ehmedli" });
+Student student = new Student
+{
+    Id = 2,
+    FirstName = "Smoke",
+    LastName = "Reynold",
+    Adress = "Ottawa" 
+};
+manager.Add(student);
+interface IPerson // konkret obyekt
 {
     int Id { get; set; }
     string FirstName { get; set; }
@@ -19,4 +31,12 @@ class Student : IPerson//mucerred obyekt
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Adress { get; set; }
+}
+
+class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
 }
